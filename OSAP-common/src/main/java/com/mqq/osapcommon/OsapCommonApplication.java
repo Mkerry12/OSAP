8ef4107e548999d31b993edac1cd5fd7f78b4137
Moserry <momokaihua@gmail.com>
2026-06-19 18:12:51 +0800

    项目pom文件初始化

---
+++ OSAP-common/src/main/java/com/mqq/osapcommon/OsapCommonApplication.java
@@ -0,0 +1,13 @@
+package com.mqq.osapcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsapCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsapCommonApplication.class, args);
+    }
+
+}
---
+++ OSAP-server/src/main/java/com/mqq/osapserver/OsapServerApplication.java
@@ -0,0 +1,13 @@
+package com.mqq.osapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsapServerApplication.class, args);
+    }
+
+}
